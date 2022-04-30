47e8a69ff89fae7462e0015b35046838c64c68c5
Martin Hans Keskküla <21132799+gCoreByte@users.noreply.github.com>
2022-05-01 02:35:26 +0300

    Update Kasulikud_protseduurid_ja_paringud.sql

--- Kasulikud_protseduurid_ja_paringud.sql
+++ Kasulikud_protseduurid_ja_paringud.sql
@@ -1,32 +1,37 @@
 #Paringud
   #Leida kõik mänguasjad, mille praegune hinnaline väärtus on mingis vahemikus.
+  #Allolevas näites 5..10 vahel
   SELECT nimi, brand, aasta FROM manguasi
   KEY JOIN hind
-  WHERE praegune<x AND praegune>y;
+  WHERE praegune<10 AND praegune>0;
 
 
   #Leida kõik mänguasjad, mille esialgne hinnaline väärtus on mingis vahemikus.
+  #Allolevas näites 0..20 vahel
   SELECT nimi, brand, aasta FROM manguasi
   KEY JOIN hind
-  WHERE algne<x AND algne>y;
+  WHERE algne<20 AND algne>0;
 
 
   #Leida kõik mänguasjad mingis kategoorias.
+  #Allolevas näites "Strateegia"
   SELECT nimi, brand, aasta FROM manguasi
-  KEY JOIN kategooria
-  WHERE kategooria = ‘[antud kategooria]’;
+  KEY JOIN manguasi_kategooria
+  WHERE kategooria = 'Strateegia';
 
 
   #Leida kõik mänguasjad, mille suurused ei ületa teatuid piire.
-  SELECT nimi, brand, aasta FROM manguasjad
+  #Allolevas näites peab olema kõik väiksem kui 5.
+  SELECT nimi, brand, aasta FROM manguasi
   KEY JOIN suurus
-  WHERE korgus<x AND laius<y AND pikkus<z AND kaal<t;
+  WHERE korgus<5 AND laius<5 AND pikkus<5 AND kaal<5;
 
 
   #Leida kõik mänguviisiga x mänguasjad.
+  #Allolevas näites mänguviisiga "Üksteise vastu".
   SELECT nimi, brand, aasta FROM manguasi
-  KEY JOIN manguviis
-  WHERE manguviis = ‘[antud manguviis]’;
+  KEY JOIN manguasi_manguviis
+  WHERE manguviis = 'Üksteise vastu';
 
 
 #Protseduurid:
@@ -60,6 +65,24 @@
       VALUES (kategooria_kategooria);
     END;
 
+  #Lisa mänguasjale tõlge.
+  CREATE OR REPLACE PROCEDURE sp_lisa_tolge(
+    IN manguasi_nimi varchar(255), IN manguasi_brand varchar(255), IN manguasi_aasta date,
+    IN tolge_keel varchar(255), IN tolge_nimi varchar(255)
+  )
+  BEGIN
+    DECLARE d_tolge_id integer;
+    DECLARE d_manguasi_id integer;
+    SELECT id INTO d_manguasi_id FROM manguasi WHERE nimi = manguasi_nimi AND brand = manguasi_brand AND aasta = manguasi_aasta;
+    IF (EXISTS (SELECT keel, nimi FROM tolge WHERE keel = tolge_keel AND nimi = tolge_nimi)) THEN    
+      SELECT id into d_tolge_id FROM tolge WHERE keel = tolge_keel AND nimi = tolge_nimi;
+      INSERT INTO manguasi_tolge (manguasi_id, tolge_id) VALUES (d_manguasi_id, d_tolge_id);
+      RETURN;
+    END IF;
+    INSERT INTO tolge (keel, nimi) VALUES (tolge_keel, tolge_nimi);
+    SELECT @@identity INTO d_tolge_id;
+    INSERT INTO manguasi_tolge (manguasi_id, tolge_id) VALUES (d_manguasi_id, d_tolge_id);
+  END;
 
   #Eemalda mänguasi, mille nimi, bränd ja väljastamisaasta on antud.
   CREATE PROCEDURE sp_eemalda_manguasi(IN nimi varchar(255), IN brand varchar(255), IN aasta date)
